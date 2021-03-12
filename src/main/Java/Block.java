a320ef565ca232e4d98f073d5496b3e7145bee60
pica-miolos <18245013+pica-miolos@users.noreply.github.com>
2021-03-12 02:38:47 +0000

    static block + blockchain with pow (missing dynamic transactions+merkleRoot)

--- src/main/Java/Block.java
+++ src/main/Java/Block.java
@@ -1,25 +1,65 @@
 import java.util.Date;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class Block {
 
-    public String hash;
-    public String previousBlockHash;
+    private int index;
+    private long timeStamp; //creation date
+    public String hash; //sha 256
+    public String previousHash;
+    public String merkleRoot;
     private String data;
+    private int nonce; //used in mining
     //public ArrayList<Transaction> transactions = new ArrayList<Transaction>();
-    private long timeStamp;
-    private int nonce;
+
+    //bitcoin wiki:
+    //int transaction counter?
+    //hashMerkleRoot
 
     //Construtor do Bloco
-    public Block(String data, String previousBlockHash) {
-        this.data = data;
+    public Block(int index, long timestamp , String previousHash,String data) {
+
+        this.index = index;
+        this.timeStamp = timestamp; //new Date().getTime();
         //this.transactions = transactions;
-        this.previousBlockHash = previousBlockHash;
-        this.timeStamp = new Date().getTime();
-        this.nonce = nonce;
-        this.hash = calculateHash();
+        this.previousHash = previousHash;
+        this.merkleRoot = merkleRoot;
+        this.data=data;
+
+        nonce=0;
+        hash=Block.calculateHash(this);
+        merkleRoot=Block.calculateHash(); // ou   merkleRoot = StringUtil.getMerkleRoot(transactions); ou das transactions?
 
     }
 
+    public int getIndex(){
+        return index;
+    }
+
+    public long getTimeStamp(){
+        return timeStamp;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getPreviousHash(){
+        return previousHash;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+    public String str() {
+        return index + timeStamp + previousHash + data + nonce;
+    }
+
+
+    /*
     //data to Hash
     public String calculateHash(){
         String newHash= Utils.applySecureHash( previousBlockHash 
@@ -27,7 +67,8 @@ public class Block {
                 + toString(nonce)
                 + data ); //merkleRoot
         return newHash;
-    }
+
+
 
 
     //dificuldade igual ao número de zeros iniciais à esquerda da hash
@@ -41,5 +82,71 @@ public class Block {
 
         System.out.println("Bloco minado:" + hash);
     }
+
+
+
+    } */
+
+
+
+
+
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Block #").append(index)
+                .append(" [previousHash : ").append(previousHash).append(", ")
+                .append("timestamp : ").append(new Date(timeStamp)).append(", ")
+                .append("data : ").append(data).append(", ")
+                .append("hash : ").append(hash).append(", ")
+                .append("merkleRoot : ").append(merkleRoot).
+                .append("]");
+        return builder.toString();
+    }
+
+
+
+    public static String calculateHash(Block block) {
+        if (block != null) {
+            MessageDigest digest = null;
+
+            try {
+                digest = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e) {
+                return null;
+            }
+
+            String txt = block.str();
+            final byte bytes[] = digest.digest(txt.getBytes());
+            final StringBuilder builder = new StringBuilder();
+
+            for (final byte b : bytes) {
+                String hex = Integer.toHexString(0xff & b);
+
+                if (hex.length() == 1) {
+                    builder.append('0');
+                }
+
+                builder.append(hex);
+            }
+
+            return builder.toString();
+        }
+
+        return null;
+    }
+
+    public void mineBlock(int difficulty) {
+        nonce = 0;
+
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+
+        while (!getHash().substring(0,  difficulty).equals(Utils.zeros(difficulty))) {
+            nonce++;
+            hash = Block.calculateHash(this);
+        }
+    }
+
+
     
 }
--- src/main/Java/Blockchain.java
+++ src/main/Java/Blockchain.java
@@ -1,38 +1,115 @@
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Blockchain {
 
-    public static ArrayList<Block> blockchain = new ArrayList<>();
-    public int difficulty = 1;
+    public static List<Block> blocks;
+    public int DIFFICULTY = 1;
 
-    // initialTransaction -> hash de initialTransaction(é única?) -> blockGenesis
-    public void createGenesis(){
-        System.out.println("Primeiro bloco da blockchain criado");
-        // Block genesis = new Block("0", genesisHash);
+
+    public Blockchain(int DIFFICULTY){
+        this.DIFFICULTY = DIFFICULTY;
+        blocks = new ArrayList<>();
+
+        Block a = new Block(0, System.currentTimeMillis(), null, "Genesis");
+        a.mineBlock(DIFFICULTY);
+        blocks.add(a);
     }
 
-    /* novaTransaction -> hash de novaTransction -> lista de transaction
-    -> novoBloco(lista de transições, nounce, hashPrevious)
 
-    public void createBlock(){
-        //new transaction ++ transactions + nounce + previousHash
+    //CREATE
+    public Block newBlock(String data) {
+        Block latestBlock = latestBlock();
+        return new Block(latestBlock.getIndex() + 1, System.currentTimeMillis(),
+                latestBlock.getHash(), data);
     }
 
-    */
-
-    public void addBlock(){
-
+    //ADD e MINING
+    public void addBlock(Block a) {
+        if (a != null) {
+            a.mineBlock(DIFFICULTY);
+            blocks.add(a);
+        }
     }
 
-    public void currentBlockHash(){
-
+    //LAST
+    public Block latestBlock() {
+        return blocks.get(blocks.size() - 1);
     }
 
-    public void validateChain(){
+    //Validate genesis block
+    public boolean isGenesisValid() {
+        Block Genesis = blocks.get(0);
 
+        if (Genesis.getIndex() != 0) {
+            return false;
+        }
+
+        if (Genesis.getPreviousHash() != null) {
+            return false;
+        }
+
+        if (Genesis.getHash() == null ||
+                !Block.calculateHash(Genesis).equals(Genesis.getHash())) {
+            return false;
+        }
+
+        return true;
     }
+
+
+    public boolean isValidNewBlock(Block newBlock, Block previousBlock) {
+        if (newBlock != null  &&  previousBlock != null) {
+            if (previousBlock.getIndex() + 1 != newBlock.getIndex()) {
+                return false;
+            }
+
+            if (newBlock.getPreviousHash() == null  ||
+                    !newBlock.getPreviousHash().equals(previousBlock.getHash())) {
+                return false;
+            }
+
+            if (newBlock.getHash() == null  ||
+                    !Block.calculateHash(newBlock).equals(newBlock.getHash())) {
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public boolean isBlockChainValid() {
+        if (!isGenesisValid()) {
+            return false;
+        }
+
+        for (int i = 1; i < blocks.size(); i++) {
+            Block currentBlock = blocks.get(i);
+            Block previousBlock = blocks.get(i - 1);
+
+            if (!isValidNewBlock(currentBlock, previousBlock)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (Block block : blocks) {
+            builder.append(block).append("\n");
+        }
+
+        return builder.toString();
+    }
+
 
 
 
